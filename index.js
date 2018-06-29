69db6f62f929b0ac9d9d6cb22a91cfbbe1d87852
Marcus Vinicius <marcus.vinicius@integritas.net>
2018-06-29 03:26:16 -0300

    showing logs only after download all pages

--- index.js
+++ index.js
@@ -5,19 +5,16 @@ const isURL = require('./lib/url.validator');
 const request = require('./lib/request');
 const chunkArray = require('./lib/array.utils').chunkArray;
 
-init(process.argv);
+const args = handleArgs(process.argv);
+init(args);
 
-function init(args) {
+function showUrlsValidationStatus(invalid, valid) {
+  console.log(color.red(`\nInvalid urls: ${invalid.length}`));
+  console.log(color.green(`Valid urls: ${valid.length}\n`));
+}
+
+function init({filePath, chunkSize}) {
   const startTime = Date.now();
-  const lastArg = args.pop();
-  let filePath, chunkSize = 50;
-  if ( isNaN(lastArg) ) {
-    filePath = lastArg;
-  } else {
-    chunkSize = Number(lastArg);
-    filePath = args.pop();
-  }
-  
   const validUrls = [];
   const invalidUrls = [];
   readFileLineByLine(
@@ -30,36 +27,73 @@ function init(args) {
       }
     }, 
     () => {
-      console.log(color.red('\nInvalid urls below\n'));
-      invalidUrls.forEach(u => console.log(color.red(u)));
-      console.log(color.green('\nGoing to download valid urls below\n'));
-      validUrls.forEach(u => console.log(color.green(u)));
-      console.log('\n\n\n');
-      
-      batch(validUrls, chunkSize, () => {
-        console.log(color.green('END'));
+      showUrlsValidationStatus(invalidUrls, validUrls);
+      batch(validUrls, chunkSize, (error, logs) => {
+        console.log('\n\n');
+        logs.forEach(l => console.log(l,'\n'));
+        console.log(color.green('\n\nEND'));
         console.log(color.green(`total time: ${endTime(startTime)}`));
       });
     }
   )
 }
 
+function handleArgs(args) {
+  const lastArg = args.pop();
+  let filePath, chunkSize = 50;
+  if ( isNaN(lastArg) ) {
+    filePath = lastArg;
+  } else {
+    chunkSize = Number(lastArg);
+    filePath = args.pop();
+  }
+  return {
+    chunkSize: chunkSize, 
+    filePath: filePath
+  };
+}
+
 function batch(urlArray, chunkSize, endCallback) {
   let chunks;
   let tasks;
   chunks = chunkArray(urlArray, chunkSize);
-  tasks = chunks.map( (urls) => (callback) => requestLinks( urls, () => callback() ) );
+  tasks = [
+    (callback) => requestLinks(chunks[0], (logs) => callback(null, logs)), 
+    ...getNextTasks(chunks.slice(1))
+  ];
   waterfall(tasks, endCallback);
+}
+
+function getNextTasks(chunks) {
+  return chunks.map(
+    (urls) => {
+      return (prevLogs, callback) => {
+        requestLinks( urls, (logs) => {
+          let nextLogs = [ ...logs ];
+          if ( prevLogs && prevLogs.length > 0 ) {
+            nextLogs = [ ...prevLogs, ...logs ];
+          }
+          callback(null, nextLogs);
+        });
+      }
+    }
+  );
+}
+
+function isFunc(functionToCheck) {
+  return functionToCheck && {}.toString.call(functionToCheck) === '[object Function]';
 }
 
 function requestLinks(validUrls, end) {
   let downloaded = 0;
+  let linksLogs = [];
   validUrls
-  .forEach( u => requestLink(u, () => {
+  .forEach( u => requestLink(u, (logs) => {
     downloaded += 1;
+    linksLogs = [ ...linksLogs, ...logs ];
     console.log(color.yellow(`download ${validUrls.length - downloaded} pages simultaneously`));
     if (downloaded === validUrls.length){
-      end();
+      end(linksLogs);
     }
   }));
 }
@@ -70,16 +104,13 @@ function requestLink(link, doneCB) {
   .then((html) => {
     const title = getTitle(html);
     const linkDetails = getLinkDetails(title, link, endTime(startTime));
-    console.log(linkDetails, '\n\n');
-    doneCB();
+    doneCB([linkDetails]);
   }, 
   (err) => {
-    console.log(color.red(failedLinkDetails(link)), '\n');
-    doneCB();
+    doneCB([color.red(failedLinkDetails(link))]);
   })
   .catch((err) => {
-    console.log(color.red(failedLinkDetails(link)), '\n');
-    doneCB();
+    doneCB([color.red(failedLinkDetails(link))]);
   });
 }
 
@@ -93,7 +124,7 @@ function getTitle(html) {
 }
 
 function getLinkDetails(title, link, time) {
-  return `${color.green(title)} : ${link} : ${color.green(time)}`;
+  return `${color.blue(title)} : ${link} : ${color.green(time)}`;
 }
 
 function failedLinkDetails(link) {
